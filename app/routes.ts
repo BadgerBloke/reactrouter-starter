8de421f78ecc96f83bd6988ba130137c737c428a
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-08-17 14:53:27 +0530

    fix: deps updated

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { flatRoutes } from 'remix-flat-routes';
 import { remixRoutesOptionAdapter } from '@react-router/remix-routes-option-adapter';
 
 export default remixRoutesOptionAdapter(defineRoutes => {
-    return flatRoutes('routes', defineRoutes, {
+    return flatRoutes('app', defineRoutes, {
         ignoredRouteFiles: ['**/.*'],
     });
 });
